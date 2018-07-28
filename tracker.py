d1b51b39c63d03f23a9960d8459d1219bdd4d0ca
Pavitra122 <pavitrashadvani122@gmail.com>
2018-07-28 20:18:02 +0530

    Code changes

---
+++ tracker.py
@@ -0,0 +1,72 @@
+# Generated the requirements.txt file by the pipreqs module from the internet.
+# Just type $ pipreqs /home/project/location to genereate the file.
+# https://github.com/bndr/pipreqs
+
+from bs4 import BeautifulSoup
+import requests
+import sys
+import json
+import time
+#from user_agent import generate_user_agent
+#from playsound import playsound
+
+url = "https://courses.illinois.edu/schedule/2018/fall/"
+
+def returnClassStatus(className, classNumber, CRN):
+	url = "https://courses.illinois.edu/schedule/2018/fall/"
+	url = url + str(className) + "/" + str(classNumber)
+
+	r = requests.get(url, timeout=5)
+	soup = BeautifulSoup(r.content,'html.parser')
+
+	datastring = soup.find_all('script')[3].text.encode("utf-8")[26:-93]
+	courses = json.loads(datastring)
+
+
+	for course in courses:
+		if int(CRN) == int(course['crn']):
+			if course['availability'] != 'Open' and course['availability'] != 'Open (Restricted)':
+				return 'Closed'
+			else:
+				return 'Open'
+	return 'Course Not found'
+
+
+
+'''
+# Sends a request to the server that appears to be sent from an actual user
+headers = {'User-Agent': generate_user_agent(device_type="desktop", os=('mac', 'linux'))}
+url = "https://courses.illinois.edu/schedule/2018/fall/"
+className = sys.argv[1]
+classNumber = sys.argv[2]
+CRN = sys.argv[3]
+url = url + str(className) + "/" + str(classNumber)
+classOpen = 0
+
+while classOpen == 0:
+
+		flag = 0
+
+		r = requests.get(url, timeout=5, headers=headers)
+		soup = BeautifulSoup(r.content,'html.parser')
+
+		datastring = soup.find_all('script')[3]
+		datastring = datastring.text
+		datastring = datastring.encode("utf-8")
+		datastring= datastring[26:-93]
+		courses = json.loads(datastring)
+
+		for course in courses:
+			if CRN == course['crn']:
+				while course['availability'] != 'Open' and course['availability'] != 'Open (Restricted)':
+					print "Hang Tight...will let you know when the course opens"
+					flag = 1
+					time.sleep(300) #Refreshes the webpage every 5 minutes
+					break
+				if flag ==1:
+					break
+				print "Course is now open"
+				classOpen =1
+				playsound('03  Hips Dont Lie [ Featuring Wycelf Jean ] - Shakira.mp3')
+				break
+'''
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,193 @@
-from flask import Flask
-application = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "<h1>Course Tracker</h1><p>A prototype API</p>"
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+from flask import request, jsonify
+import tracker
+import sys, os, traceback
+import time
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import threading
+
+app = Flask(__name__)
+
+
+#sheet.update_cell(1, 1, "My name is pavitra")
+#row = ["I'm","inserting","a","row","into","a,","Spreadsheet","with","Python"]
+#index = 2
+#sheet.delete_row(index)
+#sheet.insert_row(row, index)
+
+
+
+
+
+@app.route('/')
+def hello_world():
+    return '<h1>Course Tracker</h1><p>A prototype API</p>'
+
+
+
+
+
+
+@app.route('/v1/', methods=['GET'])
+def resources():
+    return jsonify(['resources'])
+
+
+
+
+#http://localhost:5000/v1/course_track/add?department=ECE&courseNumber=120&CRN=65253
+#http://localhost:5000/v1/course_track/add?department=ECE&courseNumber=120&CRN=64598
+
+@app.route('/v1/course_track/add', methods=['GET'])
+def add():
+    try:
+        if 'courseNumber' in request.args:
+            courseNumber = int(request.args['courseNumber'])
+        if 'department' in request.args:
+            department = request.args['department']
+        if 'CRN' in request.args:
+            CRN = int(request.args['CRN'])
+
+        for i in range(len(trackedClasses)):
+            if int(trackedClasses[i]['CRN']) == int(CRN):
+                trackedClasses[i]['users'] = trackedClasses[i]['users'] + 1
+                #sheet.delete_row(i+2)
+                sheet.update_cell(i+2,4,trackedClasses[i]['users'] )
+                #sheet.insert_row([ trackedClasses[i]['department'], trackedClasses[i]['courseNumber'],trackedClasses[i]['CRN'],trackedClasses[i]['users'],trackedClasses[i]['status'] ], i+2)
+                return 'Increased number of users, new number = ' + str(trackedClasses[i]['users'])
+
+
+        if tracker.returnClassStatus(department,courseNumber,CRN) != 'Course Not found' :
+            trackedClasses.append({'department':department, 'courseNumber':courseNumber, 'CRN':CRN  , 'users':1, 'status':tracker.returnClassStatus(department,courseNumber,CRN)})
+            sheet.append_row([department, courseNumber, CRN  , 1, tracker.returnClassStatus(department,courseNumber,CRN)])
+            return 'Added new course'
+        else:
+           return 'Course Not found'
+    except Exception as e:
+        print e
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        tb = traceback.extract_tb(exc_tb)[-1]
+        print(exc_type, tb[2], tb[1])
+
+
+
+
+
+
+#http://localhost:5000/v1/course_track/delete?department=ECE&courseNumber=120&CRN=65253
+#http://localhost:5000/v1/course_track/delete?department=ECE&courseNumber=120&CRN=64598
+
+@app.route('/v1/course_track/delete', methods=['GET'])
+def remove():
+    if 'courseNumber' in request.args:
+        courseNumber = int(request.args['courseNumber'])
+    if 'department' in request.args:
+        department = request.args['department']
+    if 'CRN' in request.args:
+        CRN = int(request.args['CRN'])
+
+    for i in range(len(trackedClasses)):
+        if int(trackedClasses[i]['CRN']) == int(CRN):
+            trackedClasses[i]['users'] = trackedClasses[i]['users'] - 1
+            if trackedClasses[i]['users'] == 0:
+                del trackedClasses[i]
+                sheet.delete_row(i+2)
+                return 'Not tracking class anymore'
+            sheet.update_cell(i+2,4,trackedClasses[i]['users'] )
+            return 'Decreased number of users, new number = ' +  str(trackedClasses[i]['users'])
+    return 'Some error occured'
+
+
+
+
+
+
+
+
+
+@app.route('/v1/course_track/returnClassStatus', methods=['GET'])
+def status():
+	try:
+		if 'courseNumber' in request.args:
+			courseNumber = int(request.args['courseNumber'])
+		if 'department' in request.args:
+			department = request.args['department']
+		if 'CRN' in request.args:
+			CRN = int(request.args['CRN'])
+		print department,courseNumber,CRN
+		return tracker.returnClassStatus(department,courseNumber,CRN)
+	except Exception as e :print(e)
+
+
+def updateLoop():
+    while 1:
+        print 'Updating'
+        time.sleep(5)
+        for i in range(len(trackedClasses)):
+            trackedClasses[i]['status'] = tracker.returnClassStatus(trackedClasses[i]['department'],trackedClasses[i]['courseNumber'], trackedClasses[i]['CRN'])
+            sheet.update_cell(i+2,5,trackedClasses[i]['status'])
+
+
+
+
+
+
+@app.route('/v1/course_track/return_list', methods = ['GET'])
+def returnArray():
+	try:
+		return jsonify(trackedClasses)
+	except Exception as e :print(e)
+
 
 if __name__ == "__main__":
-    application.run()
+
+    trackedClasses= []
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+
+    creds = ServiceAccountCredentials.from_json_keyfile_name('Project-f939c591cfa1.json', scope)
+    client = gspread.authorize(creds)
+    print "a"
+    sheet = client.open("courses").sheet1
+    trackedClasses = sheet.get_all_records()
+
+    t1 = threading.Thread(target=updateLoop, args=())
+    t1.start()
+    app.run()
+    t1.join()
+
+
+
+
+'''
+app.run()
+while 1:
+    print 'Hello'
+    time.sleep(5)
+    for i in range(len(trackedClasses)):
+        trackedClasses[i]['status'] = tracker.returnClassStatus(trackedClasses[i]['department'],trackedClasses[i]['courseNumber'], trackedClasses[i]['CRN'])
+        sheet.update_cell(i+2,5,trackedClasses[i]['status'])
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#abcd
